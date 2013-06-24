922546e45dbf81dc5b9086f12026c222ce7aa675
Andreas Hauser <andreas.hauser@wikitude.com>
2013-06-24 10:22:59 +0200

    Increases Version number and adds sample app license key

--- WikitudeTitaniumSample/Resources/ui/windows/MainWindow.js
+++ WikitudeTitaniumSample/Resources/ui/windows/MainWindow.js
@@ -11,7 +11,7 @@ function MainWindow() {
 	 * 
 	 * http://www.wikitude.com/products/wikitude-sdk/pricing/
 	 */ 
-	var WikitudeLicenseKey = "TODO";
+	var WikitudeLicenseKey = "kDc867UKvVb4YuftUQio+qnZAWWyXBy6sYPisyGa7zWb6Tn3Ot3MpH6k8JXfGArpibd/dTLVpIchtwpXLSrE2tXgqH7dWnnUMryhUXzejWY0Nr3E0p3wq/4BGeOvkP3vEp9o8OQx3uwy8rSeqLbU7s7c0yXYOQXR3ghyLt6yQR1TYWx0ZWRfX2eVB3YBo0fXnhFQ53ghQoYtMhjgz2Cdt6dR2OvjpY/PQU7oNVYZH6RYl6KydgIR5hNzMDsh5k8QixEMi/zJzXtvY5WLnCS97s8zH+BqtD2iRJRk/pbHyHFz7mDKWUgOJeAoYky69lwSqfMMyFdwFKc455OCguJmrAOMk5FO+M6eg3xwEJtcYtp+3qq9nv405y6wroo64aG/wiDXLTBct+bQc3zeFs8hvMbFteHkJ+ubhV7N+i4NrnOAx+Cc4oDxyjdicyVWmxIDfYUDq4KFA/kEa4vhC8dS9iqeEvWh5eHV9kHwsVRfwnG17mmPnQifgxbhK+cQ2Rqpp1mADcaeetKdHJYLXl7mnJHaGsFuEDQpJYlXEzmGtNmM4wtCh8mcl8lLcfnxKgx/OfbzbV8bP74EFcgtTGM7H31Hb83GOryeQnngoqXl/3vAjklErQNWHE+eBJfYf9UCu60LXTrHDBwvGIO/kr3wliwgu4DAgo7I72gTtb8iVX6m8XBReyCYNCtADWGTmrknSGaNDGTbZzp0ZwEKLwnVG0s8g45aCpUYjerBsHe1XjQ=";
 	
 	var windowTitle = 'Wikitude Module Samples';
 
@@ -39,7 +39,7 @@ function MainWindow() {
 					{
 						windowTitle : '3D and Image Recognition',
 						samples: [ 
-							{ title: '1. Model on Target', 		file: '2_3dAndImageRecognition_1_3dModelOnTarget/index.html' },
+							{ title: '1. 3D Model on Target', 		file: '2_3dAndImageRecognition_1_3dModelOnTarget/index.html' },
 							{ title: '2. Appearing Animation', 	file: '2_3dAndImageRecognition_2_AppearingAnimation/index.html' },
 							{ title: '3. Interactivity', 		file: '2_3dAndImageRecognition_3_Interactivity/index.html'}
 						]
@@ -62,7 +62,7 @@ function MainWindow() {
 						windowTitle : 'Obtain POI data',
 						samples: [ 
 							{ title: '1. Load from webservice', 		file: '4_ObtainPoiData_1_FromWebservice/index.html' },
-							{ title: '2. Load from Local Resource', 	file: '4_ObtainPoiData_2_FromLocalResource/index.html' }
+							{ title: '2. Load from local resource', 	file: '4_ObtainPoiData_2_FromLocalResource/index.html' }
 						]
 					},
 
