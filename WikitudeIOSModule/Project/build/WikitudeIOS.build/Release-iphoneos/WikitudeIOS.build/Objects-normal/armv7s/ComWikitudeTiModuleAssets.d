b08cfa65bd43ae6da57927ce34612756141266fd
Andreas Schacherbauer <andreas.schacherbauer@wikitude.com>
2013-08-29 11:01:34 +0200

    Updates .gitignore;
    Signed-off-by: Andreas Schacherbauer <andreas.schacherbauer@wikitude.com>

--- WikitudeIOSModule/Project/build/WikitudeIOS.build/Release-iphoneos/WikitudeIOS.build/Objects-normal/armv7s/ComWikitudeTiModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Andi/Development/Wikitude/GitHub/Titanium/WikitudeIOSModule/Classes/ComWikitudeTiModuleAssets.m \
-  /Users/Andi/Development/Wikitude/GitHub/Titanium/WikitudeIOSModule/Classes/ComWikitudeTiModuleAssets.h
