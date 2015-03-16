4e4bd441a6b65be20f207cb693c019cb5cbe1be6
Andreas Schacherbauer <andreas.schacherbauer@wikitude.com>
2015-03-16 13:39:59 +0100

    Adds Android examples again;
    Signed-off-by: Andreas Schacherbauer <andreas.schacherbauer@wikitude.com>

--- WikitudeTitaniumSample/Resources/android/assets/3_3dModels_2_AppearingAnimation/js/appearinganimation.js
+++ WikitudeTitaniumSample/Resources/android/assets/3_3dModels_2_AppearingAnimation/js/appearinganimation.js
@@ -1,5 +1,6 @@
 var World = {
 	loaded: false,
+	rotating: false,
 	trackableVisible: false,
 
 	init: function initFn() {
@@ -8,12 +9,12 @@ var World = {
 
 	createOverlays: function createOverlaysFn() {
 		/*
-			First an AR.Tracker needs to be created in order to start the recognition engine. It is initialized with a URL specific to the target collection. Optional parameters are passed as object in the last argument. In this case a callback function for the onLoaded trigger is set. Once the tracker is fully loaded the function loadingStep() is called.
+			First an AR.ClientTracker needs to be created in order to start the recognition engine. It is initialized with a URL specific to the target collection. Optional parameters are passed as object in the last argument. In this case a callback function for the onLoaded trigger is set. Once the tracker is fully loaded the function loadingStep() is called.
 
 			Important: If you replace the tracker file with your own, make sure to change the target name accordingly.
 			Use a specific target name to respond only to a certain target or use a wildcard to respond to any or a certain group of targets.
 		*/
-		this.tracker = new AR.Tracker("assets/tracker.wtc", {
+		this.tracker = new AR.ClientTracker("assets/tracker.wtc", {
 			onLoaded: this.loadingStep
 		});
 
@@ -24,17 +25,12 @@ var World = {
 
 			A function is attached to the onLoaded trigger to receive a notification once the 3D model is fully loaded. Depending on the size of the model and where it is stored (locally or remotely) it might take some time to completely load and it is recommended to inform the user about the loading time.
 		*/
-		this.modelCar = new AR.Model("assets/car_animated.wt3", {
+		this.modelCar = new AR.Model("assets/car.wt3", {
 			onLoaded: this.loadingStep,
-			/*
-				The drawables are made clickable by setting their onClick triggers. Click triggers can be set in the options of the drawable when the drawable is created. Thus, when the 3D model onClick: this.toggleAnimateModel is set in the options it is then passed to the AR.Model constructor. Similar the button's onClick: this.toggleAnimateModel trigger is set in the options passed to the AR.ImageDrawable constructor. toggleAnimateModel() is therefore called when the 3D model or the button is clicked.
-
-				Inside the toggleAnimateModel() function, it is checked if the animation is running and decided if it should be started, resumed or paused.
-			*/
 			scale: {
-				x: 0.5,
-				y: 0.5,
-				z: 0.5
+				x: 0.0,
+				y: 0.0,
+				z: 0.0
 			},
 			translate: {
 				x: 0.0,
@@ -43,41 +39,6 @@ var World = {
 			},
 			rotate: {
 				roll: -25
-			},
-			onClick : function( drawable, model_part ) {
-				switch(model_part) 
-				{
-					case 'WindFL': 
-					case 'DoorL[0]':
-					case 'DoorL[1]':
-					case 'DoorL[2]':
-					case 'DoorL[3]':
-						var anim = new AR.ModelAnimation(World.modelCar, "DoorOpenL_animation");
-						anim.start();
-						break;
-
-					case 'WindFR': 
-					case 'DoorR[0]':
-					case 'DoorR[1]':
-					case 'DoorR[2]':
-					case 'DoorR[3]':
-						var anim = new AR.ModelAnimation(World.modelCar, "DoorOpenR_animation");
-						anim.start();
-						break;
-
-					case 'Rear[0]':
-					case 'Rear[1]':
-					case 'WindR1[0]':
-					case 'WindR1[1]':
-						var anim = new AR.ModelAnimation(World.modelCar, "EngineWindow_animation");
-						anim.start();
-						break;
-
-					case 'Hood':
-						var anim = new AR.ModelAnimation(World.modelCar, "Trunkopen_animation");
-						anim.start();
-						break;
-				}
 			}
 		});
 
@@ -88,8 +49,6 @@ var World = {
 
 		/*
 			To receive a notification once the image target is inside the field of vision the onEnterFieldOfVision trigger of the AR.Trackable2DObject is used. In the example the function appear() is attached. Within the appear function the previously created AR.AnimationGroup is started by calling its start() function which plays the animation once.
-
-			To add the AR.ImageDrawable to the image target together with the 3D model both drawables are supplied to the AR.Trackable2DObject.
 		*/
 		var trackable = new AR.Trackable2DObject(this.tracker, "*", {
 			drawables: {
@@ -108,16 +67,12 @@ var World = {
 				World.appearingAnimation.start();
 			}
 			
-			
+						
 			var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
 			var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
-			var cssDivBottom = " style='width:90%; display: table-cell;vertical-align: middle; text-align: middle;'";
 			document.getElementById('loadingMessage').innerHTML =
-				"<div style='width:100%'>"+
-					"<div" + cssDivLeft + ">Scan CarAd Tracker Image:</div>" +
-					"<div" + cssDivRight + "><img src='assets/car.png'></img></div>" +
-				"<div>"+
-				"<div" + cssDivBottom + ">Click the doors, the hood or the trunk to start the animations.</div>";
+				"<div" + cssDivLeft + ">Scan CarAd ClientTracker Image:</div>" +
+				"<div" + cssDivRight + "><img src='assets/car.png'></img></div>";
 
 			// Remove Scan target message after 10 sec.
 			setTimeout(function() {
@@ -148,20 +103,13 @@ var World = {
 
 	appear: function appearFn() {
 		World.trackableVisible = true;
-		if ( World.loaded ) {
-			// Resets the properties to the initial values.
-			World.resetModel();
-			World.appearingAnimation.start();		
+		if (World.loaded) {
+			World.appearingAnimation.start();
 		}
 	},
 	disappear: function disappearFn() {
 		World.trackableVisible = false;
-	},
-
-	resetModel: function resetModelFn() {
-		World.modelCar.rotate.roll = -25;
-	},
-
+	}
 };
 
 World.init();
